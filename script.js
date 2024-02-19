bec1297dffcd1b4b81d13022fce5ad047d975418
Vincenzo <58110589+LrdVnz@users.noreply.github.com>
2024-02-19 22:52:41 +0100

    Basic layout

---
+++ script.js
@@ -0,0 +1,25 @@
+/* Marketplace di prova. Niente funzione di pagamento. 
+1. Crea un frontend dove mostrare tutti i prodotti.
+2. Crea un back end in cui aggiungere nuovi prodotti e modificare quelli esistenti. 
+3. Crea una pagina di dettaglio del prodotto. 
+
+Piano : 
+
+1. Frontend base (es. libri! )
+   - Casella di input di tipo search.
+     - Vicino alla casella di search ci sarà la casella di creazione di un nuovo prodotto. 
+   - Spazio per i contenuti creati dinamicamente. 
+   - Ogni contenuto / articolo sarà in una card. 
+     - Ogni card avrà spazio per tasti di modifica e delete. 
+   - Pagina di dettaglio. 
+
+2. Backend : 
+   - Fetch base per renderizzare i contenuti iniziali. 
+     - Funzione di ricerca contenuto / articolo. (live search)
+   - Funzioni collegate all'api :  
+     - Funzione di creazione nuovo prodotto. 
+     - Funzione di cancellazione prodotto. 
+   - Creazione di pagina di dettaglio.   
+*/
+
+
